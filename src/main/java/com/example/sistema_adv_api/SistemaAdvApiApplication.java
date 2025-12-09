200f05259ceb9c663ffb4a114571c9a18d5abd91
Paulo Mesquita <paulomesquita0@gmail.com>
2025-12-09 08:22:29 -0300

    1

---
+++ src/main/java/com/example/sistema_adv_api/SistemaAdvApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistema_adv_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAdvApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAdvApiApplication.class, args);
+	}
+
+}
